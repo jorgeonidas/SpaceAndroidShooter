a28d78dc36764add948d5b51b985c1bebce0d592
Jorge Michele Hernandez Bucci <zangetsujm@gmail.com>
2015-08-17 16:08:35 +0000

    #reconocimiento de plataforma PC, Web y android

--- ProjectNebulaSurfer/Assets/Scripts/PlayerMovement.cs
+++ ProjectNebulaSurfer/Assets/Scripts/PlayerMovement.cs
@@ -27,12 +27,20 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	public void movimiento(){
+		//cuando este en standalone o web player
+		#if UNITY_STANDALONE || UNITY_WEBPLAYER
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		if(Input.GetButtonDown("Fire1"){
+			disparar();
+		}
+		//cuando estoy en android
+		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 		float moveHorizontal = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
 		float moveVertical = CrossPlatformInputManager.GetAxisRaw ("Vertical");
-		
+		#endif		
 		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f );
 		playerRgb.velocity = movement * velocidad;
-
 		playerRgb.transform.position = new Vector2
 			(
 				Mathf.Clamp(playerRgb.transform.position.x,limiteMov.xMin, limiteMov.xMax),
