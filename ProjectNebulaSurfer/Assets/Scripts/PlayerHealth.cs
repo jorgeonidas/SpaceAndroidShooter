195a43de4b7677333042412bc9ecfcb2daa6a88b
Jorge Michele Hernandez Bucci <zangetsujm@gmail.com>
2015-08-18 16:11:07 +0000

    #powerUp escudo de energia y su respectivo efecto listo

--- ProjectNebulaSurfer/Assets/Scripts/PlayerHealth.cs
+++ ProjectNebulaSurfer/Assets/Scripts/PlayerHealth.cs
@@ -6,8 +6,14 @@ public class PlayerHealth : MonoBehaviour {
 	public GameObject explosion;
 	private LevelManager manager;
 	//public GameObject enemyExplosion;
-	// Use this for initialization
+	//escudo de energia
+	public GameObject energyShield;
+	private bool shielded;
+	public int shieldHits;
+	private int currShieldHits;
+
 	void Start () {
+		shielded = false;
 		GameObject lvlManagerObj = GameObject.FindGameObjectWithTag ("GameController");
 		manager = lvlManagerObj.GetComponent<LevelManager> ();
 	}
@@ -29,13 +35,28 @@ public class PlayerHealth : MonoBehaviour {
 			}
 		}
 		if (other.tag == "Enemy") {
-			hits -= 1;
-			if( hits <= 0){
-				Debug.Log ("gameOver");
-				manager.SetGameOver();
-				Instantiate(explosion, transform.position, transform.rotation);
-				Destroy (gameObject);
+			if (shielded == false){
+				hits -= 1;
+				if (hits <= 0) {
+					Debug.Log ("gameOver");
+					manager.SetGameOver ();
+					Instantiate (explosion, transform.position, transform.rotation);
+					Destroy (gameObject);
+				}
+			}else{//tengo escudo activado
+				currShieldHits -= 1;
+				if(currShieldHits <= 0){
+					shielded = false; // se desactiva el escudo
+					energyShield.SetActive(false);
+				}
 			}
 		}
+		//aca detectaremos cada powerup y lo instanciaremos
+		if (other.tag == "PowerUpShield") {
+			energyShield.SetActive(true);
+			currShieldHits = shieldHits;
+			shielded = true;//el escudo esta activado
+		}
+	
 	}
 }
---
+++ ProjectNebulaSurfer/Assets/Scripts/PowerUpContact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpContact : MonoBehaviour {
+	public GameObject powUpContact;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.tag == "Player") {
+			Instantiate(powUpContact, transform.position, transform.rotation);
+			//Instantiate(playerExplosion, transform.position, transform.rotation);
+			//Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
