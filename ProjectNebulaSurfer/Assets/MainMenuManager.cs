a2c2f4fa00287fec9e3e340fbd9480bb49fa9673
Jorge Michele Hernandez Bucci <zangetsujm@gmail.com>
2015-08-31 09:00:18 +0000

    #MainMenu, falta codificarlo

---
+++ ProjectNebulaSurfer/Assets/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+	public GameObject startCanvas;
+	public GameObject levelSelectCanvas;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ToLeveLSelect(){
+		startCanvas.SetActive (false);
+		levelSelectCanvas.SetActive (true);
+	}
+}
