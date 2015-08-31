9e08aaa211d8a05468b1fc108fe2e48e002901a0
Jorge Michele Hernandez Bucci <zangetsujm@gmail.com>
2015-08-31 16:59:17 +0000

    #main menu funciones basicas listas
    falta el unlockingLevel system

--- ProjectNebulaSurfer/Assets/MainMenuManager.cs
+++ ProjectNebulaSurfer/Assets/MainMenuManager.cs
@@ -4,17 +4,34 @@ using System.Collections;
 public class MainMenuManager : MonoBehaviour {
 	public GameObject startCanvas;
 	public GameObject levelSelectCanvas;
+	public GameObject QuitCanvas;
 	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if(startCanvas.activeSelf){
+				Debug.Log ("estoy en inicio");
+				QuitCanvas.SetActive(true);
+				startCanvas.SetActive(false);
+
+			}
+			if(levelSelectCanvas.activeSelf){
+				Debug.Log("estoy sleccion de nivel");
+				startCanvas.SetActive (true);
+				levelSelectCanvas.SetActive (false);
+			}
+		}
 	}
 	public void ToLeveLSelect(){
 		startCanvas.SetActive (false);
 		levelSelectCanvas.SetActive (true);
+	}
+	public void OnPushYes(){
+		Application.Quit ();
+	}
+	public void OnPushNo(){
+		QuitCanvas.SetActive(false);
+		startCanvas.SetActive(true);
 	}
 }
